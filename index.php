8d888f4433995d21184afb9f3b7074e52b6d4f60
velascojayc <jaychristopher.velasco@gmail.com>
2014-01-26 09:57:39 +0800

    Clever Box Events Website
    Coming soon...

---
+++ index.php
@@ -0,0 +1,35 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel=Stylesheet href="bootstrap/css/bootstrap.css" />
+        <link rel=Stylesheet href="bootstrap/css/bootstrap-responsive.css" />
+        <script src="js/jquery.js"></script>
+        <script src="bootstrap/js/bootstrap.js"></script>
+        <link rel="Stylesheet" href="css/cleverbox.css"/>
+        <title></title>
+    </head>
+    <body style="background-color: #fff;">
+        <div class="hero-unit">
+            <div class="row-fluid">
+                <div class="span1"></div>
+                <div class="span10">
+                    <p class="brand-to-life" style="text-align:center;">BRINGING YOUR <strong>BRAND</strong> TO LIFE</p>
+                    <div class="row-fluid">
+                        <div class="span4"></div>
+                        <div class="span4">                           
+                            <p><img src="img/cbe-nobg.png"/></p>
+                        </div>
+                        <div class="span4"></div>
+                    </div> 
+                    <p class="brand-to-life" style="text-align:center;">comming soon...</p>
+                </div>
+                <div class="span1"></div>
+            </div>
+        </div>
+    </body>
+</html>
